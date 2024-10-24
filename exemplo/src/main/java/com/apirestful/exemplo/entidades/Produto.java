9a7fc95642ffca119e9ae5272c7771b217ce753e
Maria Rita Lustosa <mritalustosa.ad@gmail.com>
2024-10-24 14:36:22 -0300

    adição de getters and setters na entidade produto

--- exemplo/src/main/java/com/apirestful/exemplo/entidades/Produto.java
+++ exemplo/src/main/java/com/apirestful/exemplo/entidades/Produto.java
@@ -24,4 +24,53 @@ public class Produto {
     )
     private List<Fornecedor> fornecedores;    
 
+    public int getIdProduto() {
+        return idProduto;
+    }
+
+    public void setIdProduto(int idProduto) {
+        this.idProduto = idProduto;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public double getPreco() {
+        return preco;
+    }
+
+    public void setPreco(double preco) {
+        this.preco = preco;
+    }
+
+    public int getQuantidade() {
+        return quantidade;
+    }
+
+    public void setQuantidade(int quantidade) {
+        this.quantidade = quantidade;
+    }
+
+    public Categoria getCategoria() {
+        return categoria;
+    }
+
+    public void setCategoria(Categoria categoria) {
+        this.categoria = categoria;
+    }
+
+    public List<Fornecedor> getFornecedores() {
+        return fornecedores;
+    }
+
+    public void setFornecedores(List<Fornecedor> fornecedores) {
+        this.fornecedores = fornecedores;
+    }
+
+
 }
