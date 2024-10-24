52033c663415811fd482e802df506883daf3d8ba
Maria Rita Lustosa <mritalustosa.ad@gmail.com>
2024-10-24 14:33:46 -0300

    implementação da entidade produto

--- exemplo/src/main/java/com/apirestful/exemplo/entidades/Produto.java
+++ exemplo/src/main/java/com/apirestful/exemplo/entidades/Produto.java
@@ -1,5 +1,27 @@
 package com.apirestful.exemplo.entidades;
 
+import java.util.*;
+import jakarta.persistence.*;
+
+@Entity
 public class Produto {
-    
+    @Id
+    @GeneratedValue(strategy=GenerationType.IDENTITY)
+    private int idProduto;
+    private String nome;
+    private double preco;
+    private int quantidade;
+
+    @ManyToOne
+    @JoinColumn(name = "idCategoria")
+    private Categoria categoria;
+
+    @ManyToMany
+    @JoinTable(
+        name = "produto_fornecedor",
+        joinColumns = @JoinColumn(name = "idProduto"),
+        inverseJoinColumns = @JoinColumn(name = "idFornecedor")
+    )
+    private List<Fornecedor> fornecedores;    
+
 }
