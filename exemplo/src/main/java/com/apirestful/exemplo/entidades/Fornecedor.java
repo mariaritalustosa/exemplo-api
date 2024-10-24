83c06abed65f0fa4f7aff0af919aa383fe9a32e3
Maria Rita Lustosa <mritalustosa.ad@gmail.com>
2024-10-24 14:21:41 -0300

    entidade fornecedor implementada

--- exemplo/src/main/java/com/apirestful/exemplo/entidades/Fornecedor.java
+++ exemplo/src/main/java/com/apirestful/exemplo/entidades/Fornecedor.java
@@ -1,5 +1,34 @@
 package com.apirestful.exemplo.entidades;
 
+import jakarta.persistence.*;
+
+@Entity
 public class Fornecedor {
+    @Id
+    @GeneratedValue(strategy=GenerationType.IDENTITY)
+    private int idFornecedor;
+    private String nome;
+    private Long contato;
+
+    public int getIdFornecedor() {
+        return idFornecedor;
+    }
+    public void setIdFornecedor(int idFornecedor) {
+        this.idFornecedor = idFornecedor;
+    }
+    public String getNome() {
+        return nome;
+    }
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+    public Long getContato() {
+        return contato;
+    }
+    public void setContato(Long contato) {
+        this.contato = contato;
+    }
+
     
+
 }
